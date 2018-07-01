f1ee06e4e550a56c948dbdfbd0a38f5fb96417a3
gmat191@gmail.com <gmat191@gmail.com>
2018-07-01 21:05:43 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0 
-  board.each do |spaces|
-    if spaces == "X" || spaces == "O"
+  board.each do |positions|
+    if positions == "X" || positions == "O"
       counter += 1
     end
 end
